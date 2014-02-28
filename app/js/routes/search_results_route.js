5dfdfe0a82b32264b43f698f378399c4a0f9f1cc
Christoph Wagner <christoph.wagner@gooddata.com>
2014-02-28 00:09:43 -0800

    Simplified code in SearchResultsRoute

--- app/js/routes/search_results_route.js
+++ app/js/routes/search_results_route.js
@@ -9,10 +9,9 @@ SpotifyExplorer.SearchResultsRoute = Ember.Route.extend({
         // query Spotify API and transform results
         Ember.$.getJSON(url).then(function(result) {
             // turn results into Ember objects
-            return result.tracks.map(function(track) {
+            var results = result.tracks.map(function(track) {
                 return SpotifyExplorer.Track.create(track);
             });
-        }).then(function(results) {
             // set as property on our model
             return search.set('results', results);
         });
