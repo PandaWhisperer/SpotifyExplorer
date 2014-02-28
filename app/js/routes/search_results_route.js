8e2d8216e6aa1e333d67952fbb6bbf9d28ea3bf3
Christoph Wagner <christoph.wagner@gooddata.com>
2014-02-27 23:48:35 -0800

    Moved Spotify API call into afterModel hook

--- app/js/routes/search_results_route.js
+++ app/js/routes/search_results_route.js
@@ -1,18 +1,20 @@
 SpotifyExplorer.SearchResultsRoute = Ember.Route.extend({
     model: function(params) {
-        return this.store.find('search', params.id).then(function(search) {
-            var url = "https://ws.spotify.com/search/1/track.json?q=" + search.get('query');
+        return this.store.find('search', params.id);
+    },
 
-            // query Spotify API and transform results
-            return Ember.$.getJSON(url).then(function(result) {
-                // turn results into Ember objects
-                return result.tracks.map(function(track) {
-                    return SpotifyExplorer.Track.create(track);
-                });
-            }).then(function(results) {
-                // set as property on our model
-                return search.set('results', results);
+    afterModel: function(search, transition) {
+        var url = "https://ws.spotify.com/search/1/track.json?q=" + search.get('query');
+
+        // query Spotify API and transform results
+        Ember.$.getJSON(url).then(function(result) {
+            // turn results into Ember objects
+            return result.tracks.map(function(track) {
+                return SpotifyExplorer.Track.create(track);
             });
+        }).then(function(results) {
+            // set as property on our model
+            return search.set('results', results);
         });
     }
 });
