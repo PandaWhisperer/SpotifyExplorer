e6b790496135cce2f21553d2d97c7a22ad6b26d9
Christoph Wagner <christoph.wagner@gooddata.com>
2014-02-27 23:59:42 -0800

    Adding debug settings

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,14 @@
 var SpotifyExplorer = window.SpotifyExplorer = Ember.Application.create({
-    appName: 'SpotifyExplorer'
+    appName: 'SpotifyExplorer',
+
+    // Basic logging, e.g. "Transitioned into 'post'"
+    LOG_TRANSITIONS: false, 
+
+    // Extremely detailed logging, highlighting every internal
+    // step made while transitioning into a route, including
+    // `beforeModel`, `model`, and `afterModel` hooks, and
+    // information about redirects and aborted transitions
+    LOG_TRANSITIONS_INTERNAL: false
 });
 
 /* Order and include as you please. */
